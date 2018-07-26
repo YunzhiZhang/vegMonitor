0329676507a8b1e65e073279e588db30101a01b1
Atreya Shankar <35427332+AtreyaSh@users.noreply.github.com>
2018-07-26 17:54:06 +0200

    new developments

---
+++ src.R
@@ -0,0 +1,31 @@
+imgList <- list.files(paste(getwd(), "/input/climg", sep=""), full.names = TRUE)[1:3]
+baseShapefile <- "./input/shp/Sum_UTM43N_4U_Classes.shp"
+bands = c(3:5)
+responseCol = NULL
+predShapefile = "./input/shp/DL_Lower_UTM43N.shp"
+undersample = NULL
+ntry = 1000
+genLogs = NULL
+writePath = NULL
+format = NULLr
+
+source("vegClassification.R", encoding = "UTF-8")
+
+options(warn = 1)
+vegClassify(imgList, baseShapefile, responseCol, predShapefile, bands, undersample, ntry, genLogs, writePath, format)
+
+### Issues ###
+
+## Priority
+
+# work on making nicer genlogs
+# undersample credit to Ali Santacruz
+
+# test function see how it works
+# add readme or package type functions
+# add documentation about format types
+# print band number in varImp log for clarity
+
+## Extra
+
+# make R-base API for python API, might be counterintuitive but might work
--- vegClassification.R
+++ vegClassification.R
@@ -12,13 +12,19 @@ library(caret)
 if(!require(snow)) install.packages("snow")
 library(snow)
 
-### function ###
+if(!require(randomForest)) install.packages("randomForest")
+library(randomForest)
 
-vegClassify <- function(imgList, baseShapefile, responseCol, predShapefile, undersample, ntry, varImp, genLogs, writePath, format) {
+if(!require(e1071)) install.packages("e1071")
+library(e1071)
+
+## function ###
+
+vegClassify <- function(imgList, baseShapefile, responseCol, predShapefile, bands, undersample, ntry, genLogs, writePath, format) {
   
   ### check dependencies ###
   
-  if(is.null(imgStack)){
+  if(is.null(imgList)){
     stop("please indicate a directory ")
   } else {
     s <- lapply(imgList, stack)
@@ -35,10 +41,14 @@ vegClassify <- function(imgList, baseShapefile, responseCol, predShapefile, unde
   
   if(is.null(predShapefile)){
     pred <- FALSE
-    warning("no prediction shapefile path with .shp provided, carrying on")
+    warning("no prediction shapefile path with .shp provided, carrying on...")
   } else {
     dLower <- shapefile(predShapefile)
     pred <- TRUE 
+  }
+  
+  if(is.null(bands)){
+    stop("please specify a vector containing the bands that should be used for training")
   }
   
   if(is.null(undersample)){
@@ -51,27 +61,27 @@ vegClassify <- function(imgList, baseShapefile, responseCol, predShapefile, unde
     warning(paste("no ntree supplied, defaulting to ", ntry, sep = ""))
   }
   
-  if(is.null(varImp)){
-    varImp <- TRUE
-    warning(paste("no varImp supplied, defaulting to ", varImp, sep = ""))
-  }
-  
   if(is.null(genLogs)){
     genLogs <- TRUE
     warning(paste("no genLogs supplied, defaulting to ", genLogs, sep = ""))
   }
   
+  if(genLogs==TRUE){
+    tmpVarImp <- list()
+    tmpResults <- list()
+    tmpPred <- list()
+  }
+  
   if(is.null(writePath)){
-    writePath = paste(getwd(), "/vegClassification", sep = "")
-    warning(paste("no write path supplied, defaulting to ", writePath, sep=""))
-    
-    if(!file.exists("./vegClassification")){
-      dir.create("./vegClassification")
+    writePath = paste(getwd(), "/output/vegClassification", sep = "")
+    warning(paste("no writePath supplied, defaulting to ", writePath, sep=""))
+  } else if (substr(writePath, nchar(writePath), nchar(writePath)) == "/") {
+    writePath <- substr(writePath, 1, nchar(writePath)-1)
+    if (!file.exists(writePath)) {
+      stop(paste("the directory ", writePath, " does not exist"))
     }
   } else if (!file.exists(writePath)) {
     stop(paste("the directory ", writePath, " does not exist"))
-  } else if (substr(writePath, nchar(writePath), nchar(writePath)) == "/") {
-    writePath <- substr(writePath, 1, nchar(writePath)-1)
   }
   
   if(is.null(format)){
@@ -96,8 +106,8 @@ vegClassify <- function(imgList, baseShapefile, responseCol, predShapefile, unde
     }
     
     image_dfall = data.frame(matrix(vector(), nrow = 0, ncol = length(names(training_image)) + 1))
-    for (i in 1:length(unique(shape_pointData[[responseCol]]))){
-      category <- unique(shape_pointData[[responseCol]])[i]
+    for (j in 1:length(unique(shape_pointData[[responseCol]]))){
+      category <- unique(shape_pointData[[responseCol]])[j]
       categorymap <- shape_pointData[shape_pointData[[responseCol]] == category,]
       dataSet <- extract(training_image, categorymap)
       dataSet <- dataSet[!unlist(lapply(dataSet, is.null))]
@@ -135,33 +145,24 @@ vegClassify <- function(imgList, baseShapefile, responseCol, predShapefile, unde
       training_bc <- undersample_ds(image_train, "class", min(table(image_train$class)))
     } else training_bc <- image_train
     
-    # Building the model with data partition
-    # experiment inputting string into command
-    ###
-    image_rf <- train(as.factor(class) ~ B1 + B2 + B3 + B4 + B5 + B6 + B7, method = "rf", data = training_bc, ntree = ntry, importance = varImp)
-    ###
+    image_rf <- train(training_bc[,bands], as.factor(training_bc[,ncol(training_bc)]), method = "rf", ntree = ntry, importance = genLogs)
     
-    saveRDS(image_rf, strsplit(names(s[[i]])[1], "[.]")[[1]][1])
-    
-    # Shows paramters like accuracy and kappa coefficient for internal OOB data
-    image_rf
-    
-    if(varImp==TRUE){
-      varImp(image_rf)[1]  
+    if(genLogs==TRUE){
+      
+      tmpVarImp[[i]] <- rbind(as.character(i), varImp(image_rf)$importance)
+      tmpResults[[i]] <- cbind(image_rf$results[which(image_rf$results[,3] == max(image_rf$results[,3])),],nrow(training_bc))
+      names(tmpResults[[i]])[length(names(tmpResults[[i]]))] <- "trainingPixels"
+      
+      imagepred_valid <- predict(image_rf, image_valid)
+      Accuracy <- confusionMatrix(imagepred_valid,as.factor(image_valid$class))$overall[1]
+      Kappa <- confusionMatrix(imagepred_valid, as.factor(image_valid$class))$overall[2]
+      byClass <- do.call("cbind", lapply(confusionMatrix(imagepred_valid, as.factor(image_valid$class))$byClass[,1], function(x) return(x)))
+      
+      tmpPred[[i]] <- cbind(Accuracy, Kappa, byClass, nrow(image_valid))
+      colnames(tmpPred[[i]])[length(colnames(tmpPred[[i]]))] <- "testPixels"
+      row.names(tmpPred[[i]]) <- NULL
     }
     
-    # Prediction on test dataset based on trained model
-    imagepred_valid <- predict(image_rf, image_valid)
-    
-    # Confusion matrix of trained model on test dataset; provides accuracy
-    confusionMatrix1 <- confusionMatrix(imagepred_valid,image_valid$class)$overall[1]
-
-    # Confusion matrix of trained model on test dataset; provides kappa coefficient
-    confusionMatrix2 <- confusionMatrix(imagepred_valid, image_valid$class)$overall[2]
-    
-    # Confusion matrix of trained model on test dataset; provides user's accuracy by class
-    confusionMatrix3 <- confusionMatrix(imagepred_valid, image_valid$class)$byClass[,1]
-      
     if(pred == TRUE){
       beginCluster()
       pred_rf <- clusterR(prediction_image, raster::predict, args = list(model = image_rf))
@@ -172,30 +173,25 @@ vegClassify <- function(imgList, baseShapefile, responseCol, predShapefile, unde
       endCluster()
     }
     
+    saveRDS(image_rf, paste(writePath, "/", strsplit(names(s[[i]])[1], "[.]")[[1]][1], ".rds", sep = ""))
     writeRaster(pred_rf, file.path(writePath, strsplit(names(s[[i]])[1], "[.]")[[1]][1]), format = format, overwrite = TRUE)
     
     Sys.sleep(1/100)
     setTxtProgressBar(pb.overall, i, title = NULL, label = NULL)
   }
+  
+  if(genLogs==TRUE){
+    tmpVarImp <- do.call("rbind", lapply(tmpVarImp, function(x) return(x)))
+    tmpResults <- do.call("rbind", lapply(tmpResults, function(x) return(x)))
+    tmpPred <- do.call("rbind", lapply(tmpPred, function(x) return(x)))
+    
+    write.csv(tmpVarImp, file.path(writePath,"log_VarImp.csv"), row.names = FALSE)
+    write.csv(tmpResults, file.path(writePath,"log_Results.csv"), row.names = FALSE)
+    write.csv(tmpPred, file.path(writePath,"log_Pred.csv"), row.names = FALSE)
+  }
+  
   end <- proc.time()
-  print(end-start)
   close(pb.overall)
+  print(end-start)
   return(0)
 }
-
-### Issues ###
-
-## Priority
-
-# issue automating inputs in train aspect
-# work on making nicer genlogs
-# check if code can automatically parse responseCol from shapefile instead of manual input
-# undersample credit to Ali Santacruz
-
-# test function see how it works
-# add readme or package type functions
-# add documentation about format types
-
-## Extra
-
-# make R-base API for python API, might be counterintuitive but might work
